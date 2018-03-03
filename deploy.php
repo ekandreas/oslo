35c73a3769f9a5004269d346d13d265fd6211a93
Andreas Ek <andreas@elseif.se>
2018-03-03 09:47:25 +0100

    deploy added

--- deploy.php
+++ deploy.php
@@ -5,13 +5,13 @@ require_once __DIR__.'/vendor/deployer/deployer/recipe/composer.php';
 
 host('185.91.65.151')
     ->port(22)
-    ->set('deploy_path', '~')
+    ->set('deploy_path', '~/public')
     ->user('wposlo_3691')
     ->set('branch', 'master')
     ->stage('production')
     ->identityFile('~/.ssh/id_rsa');
 
-set('repository', 'git@github.com:ekandreas/oslo.git');
+set('repository', 'https://github.com/ekandreas/oslo.git');
 
 set('keep_releases', 10);
 set('shared_dirs', ['web/app/uploads']);
